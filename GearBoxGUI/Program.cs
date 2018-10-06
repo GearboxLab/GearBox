e723e3ce7410303cc4a2f6be22bfe9030e535c69
Leo <leo.on.the.earth@gmail.com>
2018-10-06 23:40:26 +0800

    Restrict to show only one GearBox window

--- GearBoxGUI/Program.cs
+++ GearBoxGUI/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace GearBox
@@ -12,6 +12,14 @@ namespace GearBox
         [STAThread]
         static void Main(string[] args)
         {
+            // Restrict to show only one GearBox window
+            Mutex m = new Mutex(false, "GearBoxGUI", out bool canCreateWindow);
+
+            if (!canCreateWindow)
+            {
+                return;
+            }
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new GearBoxGUI(args));
